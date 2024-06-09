cafdd76fd8340889527894a7bee4527901860fd5
rohan <rohannooniwal39@gmail.com>
2024-06-09 12:26:17 +0530

    Update Options component and index.js

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,6 +1,7 @@
 function Options({question}) {
     return (
         <div className="option">
+            <h2>these are the options</h2>
         {question.options.map((option) => (
           <button className="btn  btn-option" key={option}>{option}</button>
         ))}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React, { StrictMode } from "react";
 import reactDom from "react-dom/client";
 import "./index.css";
-import App from "./components/App"  ;
-
+import App from "./components/App";
+// import reportWebVitals from "./reportWebVitals";
 const root = reactDom.createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
     <App />
   </StrictMode>
 );
+// reportWebVitals();
